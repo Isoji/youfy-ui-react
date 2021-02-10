69b2f36e7256891628b46d438ddfc2f3abc8aee0
imrishit98 <imrishit98@gmail.com>
2021-02-10 18:15:20 -0500

    added YouTube song name detection

--- src/components/SongInfo.js
+++ src/components/SongInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 //images
 import AlbumArt from '../images/song-album.jpg';
@@ -9,33 +9,72 @@ import Related from './Related';
 import { Link } from 'react-router-dom';
 
 const SongInfo = () => {
+  const [songName, setSongName] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  const getTitle = () => {
+    url &&
+      fetch(url)
+        .then((response) => response.text())
+        .then((html) => {
+          const doc = new DOMParser().parseFromString(html, 'text/html');
+          const title = doc.querySelectorAll('title')[0];
+          setSongName(title.innerText);
+          return title.innerText;
+        });
+  };
+
+  useEffect(() => {
+    getTitle();
+    /* eslint-disable */
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      setUrl(tabs[0].url);
+    });
+  }, [url]);
+
   return (
     <div className='song-info'>
-      <div className='song-container'>
-        <img src={AlbumArt} alt='' className='song-img' />
-        <div className='song-details'>
-          <span className='song-type'>SINGLE</span>
-          <h1 className='song-title'>Don't Let Me Down</h1>
-          <p className='song-artist'>By The Chainsmokers - 2016</p>
-          <p className='song-duration'>3 min 28 sec</p>
-          <div className='btns'>
-            <a className='link-btn' href='https://spotify.com'>
-              <img src={PlayIcon} alt='' className='btn-icon' />
-              <span className='btn-text'>Play</span>
-            </a>
-            <Link to='/add-to-playlist' className='link-btn'>
-              <img src={PlusIcon} alt='' className='btn-icon' />
-              <span className='btn-text'>Add to Playlist</span>
-            </Link>
-            <a className='link-btn' href='https://youfyapp.com'>
-              <img src={MoreIcon} alt='' className='btn-icon' />
-              <span className='btn-text'>More</span>
-            </a>
+      {songName ? (
+        <>
+          <div className='song-container'>
+            <img src={AlbumArt} alt='' className='song-img' />
+            <div className='song-details'>
+              <span className='song-type'>SINGLE</span>
+              <h1 className='song-title'>{songName}</h1>
+              <p className='song-artist'>By The Chainsmokers - 2016</p>
+              <p className='song-duration'>3 min 28 sec</p>
+              <div className='btns'>
+                <a className='link-btn' href='https://spotify.com'>
+                  <img src={PlayIcon} alt='' className='btn-icon' />
+                  <span className='btn-text'>Play</span>
+                </a>
+                <Link to='/add-to-playlist' className='link-btn'>
+                  <img src={PlusIcon} alt='' className='btn-icon' />
+                  <span className='btn-text'>Add to Playlist</span>
+                </Link>
+                <a className='link-btn' href='https://youfyapp.com'>
+                  <img src={MoreIcon} alt='' className='btn-icon' />
+                  <span className='btn-text'>More</span>
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='line'></div>
-      <Related />
+          <div className='line'></div>
+          <Related />
+        </>
+      ) : (
+        <h1
+          style={{
+            width: '574px',
+            height: '320px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          Loading...
+        </h1>
+      )}
     </div>
   );
 };

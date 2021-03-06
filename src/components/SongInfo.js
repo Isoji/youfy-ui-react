ea785090edddc095e6ca3007eb85850363d17516
imrishit98 <imrishit98@gmail.com>
2021-03-06 01:27:13 -0500

    added SongsInfo functionality v1

--- src/components/SongInfo.js
+++ src/components/SongInfo.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { formatTitle, toMinutes } from '../utils/formats';
+import Layout from './Layout';
 
 //images
 import AlbumArt from '../images/song-album.jpg';
@@ -6,14 +10,24 @@ import PlayIcon from '../images/play.svg';
 import PlusIcon from '../images/plus.svg';
 import MoreIcon from '../images/more.svg';
 import Related from './Related';
-import { Link } from 'react-router-dom';
-
-import { formatTitle } from '../utils/formats';
-import Layout from './Layout';
 
 const SongInfo = () => {
+  const token =
+    'BQDDr1-oJiobyr6xXRp2hEc7yogK1Qk25zJDdwMqMuZgB3W242a7PZCcsLT4P4OleNHYU4PXZvB3DzKyKv5Ij4KuHRsjf8CwQHo_UI-7VfrKfSEF7PjilcBbz9VIaW9Q63G8NOgMMwrPyuGqcBmsCoRaY2dB09niUcmQL5a0ValioaPyc6Fap1r5JMyhk5vv1JBidcFdQ0P5FugL56EMR_FVaMoH2V9MkhJ5-61umhxHQWWA_i96x6MqXiqUcyigs3BjD3tZGQ';
+
   const [songName, setSongName] = useState(null);
+  const [songs, setSongs] = useState([]);
   const [url, setUrl] = useState(null);
+
+  const songQueryConfig = {
+    method: 'get',
+    url: `https://api.spotify.com/v1/search?q=${songName}&type=track&limit=10`,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
   const getTitle = () => {
     url &&
@@ -23,37 +37,55 @@ const SongInfo = () => {
           const doc = new DOMParser().parseFromString(html, 'text/html');
           const title = doc.querySelectorAll('title')[0];
           setSongName(formatTitle(title.innerText));
-          return title.innerText;
+        });
+  };
+
+  const getSong = async () => {
+    songName &&
+      axios(songQueryConfig)
+        .then(function (response) {
+          console.log(response.data.tracks.items[0]);
+          setSongs(response.data.tracks.items);
+        })
+        .catch(function (error) {
+          console.log(error);
         });
   };
 
   useEffect(() => {
     getTitle();
+    getSong();
+    console.log(songName);
     /* eslint-disable */
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       setUrl(tabs[0].url);
     });
-  }, [url]);
+  }, [url, songName]);
 
   return (
     <div className='song-info'>
-      {songName ? (
+      {songs.length > 0 ? (
         <>
           <Layout>
             <div className='song-container'>
-              <img src={AlbumArt} alt='' className='song-img' />
+              <img
+                src={songs[0].album.images[0].url}
+                alt=''
+                className='song-img'
+              />
               <div className='song-details'>
-                <span className='song-type'>SINGLE</span>
-                <h1 className='song-title'>{songName}</h1>
-                <p className='song-artist'>By The Chainsmokers - 2016</p>
-                <p className='song-duration'>3 min 28 sec</p>
+                {/* <span className='song-type'>{songs[0].type}</span> */}
+                <h1 className='song-title'>{songs[0].name}</h1>
+                <p className='song-artist'>{songs[0].artists[0].name}</p>
+                <p className='song-duration'>
+                  {toMinutes(songs[0].duration_ms)}
+                </p>
                 <div className='btns'>
                   <a
                     className='link-btn'
                     target='_blank'
-                    href={`https://open.spotify.com/search/${encodeURI(
-                      songName
-                    )}`}
+                    rel='noreferrer'
+                    href={songs[0].external_urls.spotify}
                   >
                     <img src={PlayIcon} alt='' className='btn-icon' />
                     <span className='btn-text'>Play</span>
@@ -69,8 +101,12 @@ const SongInfo = () => {
                 </div>
               </div>
             </div>
-            <div className='line'></div>
-            <Related />
+            {songs.length >= 2 ? (
+              <>
+                <div className='line'></div>
+                <Related songs={songs} />
+              </>
+            ) : null}
           </Layout>
         </>
       ) : (
--- src/utils/formats.js
+++ src/utils/formats.js
@@ -54,3 +54,10 @@ export const formatTitle = (title) => {
 
   return title;
 };
+
+export const toMinutes = (millis) => {
+  millis = parseInt(millis);
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ' min ' + seconds + ' sec';
+};

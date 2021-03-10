2bce486127aa422ccf5e5bae256cf1231867191f
imrishit98 <imrishit98@gmail.com>
2021-03-10 02:21:16 -0500

    added success message to create new playlist

--- src/pages/AddNewPlaylist.js
+++ src/pages/AddNewPlaylist.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import { token } from '../utils/gets';
 import Layout from '../components/Layout';
@@ -12,6 +13,8 @@ const AddNewPlaylist = () => {
   const userId = 'yilxfvdt11z0c1myy94cyz6n6';
   const [plName, setPlName] = useState(null);
   const [plDesc, setPlDesc] = useState(null);
+  const [playlistCreated, setPlaylistCreated] = useState(false);
+  const history = useHistory();
 
   const handlePlName = (e) => {
     setPlName(e.target.value);
@@ -40,6 +43,7 @@ const AddNewPlaylist = () => {
   const createPlaylist = () => {
     axios(config)
       .then((response) => {
+        setPlaylistCreated(true);
         console.log(response.data);
       })
       .catch((error) => {
@@ -47,37 +51,55 @@ const AddNewPlaylist = () => {
       });
   };
 
+  useEffect(() => {
+    if (playlistCreated) {
+      setTimeout(() => {
+        setPlaylistCreated(false);
+        history.push(`/add-to-playlist`);
+      }, 4000);
+    }
+  }, [playlistCreated]);
+
   return (
     <>
       <Layout>
         <div className='add-new-playlist'>
-          <h1>New Playlist</h1>
-          <div className='new-playlist-form'>
-            <img src={PlaylistImg} alt='' />
-            <div className='text'>
-              <input
-                type='text'
-                placeholder='My Awesome Playlist'
-                className='inputs title'
-                onChange={handlePlName}
-              />
-              <textarea
-                placeholder='Description goes here ...'
-                className='inputs desc'
-                onChange={handlePlDesc}
-              ></textarea>
-              <div className='btns'>
-                <Link onClick={createPlaylist} className='link-btn'>
-                  <img src={Checkmark} alt='' className='btn-icon' />
-                  <span className='btn-text'>Create</span>
-                </Link>
-                <Link to='/add-to-playlist' className='link-btn'>
-                  <img src={Cross} alt='' className='btn-icon' />
-                  <span className='btn-text'>Cancel</span>
-                </Link>
+          {playlistCreated ? (
+            <>
+              <img src={Checkmark} alt='' className='success-icon' />
+              <h1 className='msg'>New playlist created successfully!</h1>
+            </>
+          ) : (
+            <>
+              <h1>New Playlist</h1>
+              <div className='new-playlist-form'>
+                <img src={PlaylistImg} alt='' />
+                <div className='text'>
+                  <input
+                    type='text'
+                    placeholder='My Awesome Playlist'
+                    className='inputs title'
+                    onChange={handlePlName}
+                  />
+                  <textarea
+                    placeholder='Description goes here ...'
+                    className='inputs desc'
+                    onChange={handlePlDesc}
+                  ></textarea>
+                  <div className='btns'>
+                    <Link onClick={createPlaylist} className='link-btn'>
+                      <img src={Checkmark} alt='' className='btn-icon' />
+                      <span className='btn-text'>Create</span>
+                    </Link>
+                    <Link to='/add-to-playlist' className='link-btn'>
+                      <img src={Cross} alt='' className='btn-icon' />
+                      <span className='btn-text'>Cancel</span>
+                    </Link>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </Layout>
     </>

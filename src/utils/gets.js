932ec5a0be390a8b8f8ca34a0c8ca3606a3c06cd
imrishit98 <imrishit98@gmail.com>
2021-03-09 19:42:24 -0500

    added gets.js to .gitignore

--- src/utils/gets.js
+++
@@ -1,7 +0,0 @@
-export const getPlId = (url) => {
-  const playlistId = url.split('=')[1];
-  return playlistId;
-};
-
-export const token =
-  'BQDALBHuIyqxRG7E4IskbKpmFV6EauvOdHnqgmcj9ITXIpdxdk1cxTYfqeVDJoPmhzUwoUhHYf3QQy7jopNwu-I0J2Yr_Ll6G8jXWzl20i38yoXmoG4sdqf1qONceK5g56lTiRmL1hvJuVjhO2CAW1v855JhrAFE4J7SSfkdMV2ZFGdXQ-r44f4NJJRSoCKM_7FypeqoPIZQ9dm6VrXJ6EcrO_2ptVo73bga2ZwB_aAdrjwZtU7nWVRhw2iSnG1lOk5kYx-ZTQ';

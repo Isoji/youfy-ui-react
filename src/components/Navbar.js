e87dd89021dd566a78ffe24c521a7916c8ab274f
imrishit98 <imrishit98@gmail.com>
2021-03-15 18:16:25 -0400

    Added authentication to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../utils/contexts';
-import { SP_GET_AUTH, SP_CODE_KEY } from '../utils/gets';
+import { SP_GET_AUTH, SP_CODE_KEY, setCookie, getCookie } from '../utils/gets';
 
 const Navbar = () => {
   const { token, setToken } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
 
   const signIn = () => {
     /* eslint-disable */
@@ -20,10 +22,42 @@ const Navbar = () => {
       }
     );
   };
+
+  const getUser = () => {
+    const config = {
+      method: 'get',
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios(config)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getCookie().then((res) => {
+      if (res) {
+        setToken(res.value);
+      }
+    });
+    if (token) {
+      setCookie(token);
+      getUser();
+    }
+  }, [token]);
+
   return (
     <nav className='navbar'>
       <h1 className='logo-text'>Youfy</h1>
-      <h2 onClick={signIn}>Sign In</h2>
+      {user ? <h2>{user.display_name}</h2> : <h2 onClick={signIn}>Sign In</h2>}
     </nav>
   );
 };
